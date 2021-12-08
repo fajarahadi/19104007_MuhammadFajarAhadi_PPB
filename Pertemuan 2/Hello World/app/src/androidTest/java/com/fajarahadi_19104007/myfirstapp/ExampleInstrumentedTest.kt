e962cab9427270719f08757a57cb79c42dd47024
Muhammad Fajar Ahadi (19104007) <72422073+fajarahadi@users.noreply.github.com>
2021-12-08 15:51:07 +0700

    Update ExampleInstrumentedTest.kt

--- Pertemuan 2/Hello World/app/src/androidTest/java/com/fajarahadi_19104007/myfirstapp/ExampleInstrumentedTest.kt
+++ Pertemuan 2/Hello World/app/src/androidTest/java/com/fajarahadi_19104007/myfirstapp/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.fauziyah_19104033.myfirstapp
+package com.fajarahadi_19104007.myfirstapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.fauziyah_19104033.myfirstapp", appContext.packageName)
+        assertEquals("com.fajarahadi_19104007.myfirstapp", appContext.packageName)
     }
 }
