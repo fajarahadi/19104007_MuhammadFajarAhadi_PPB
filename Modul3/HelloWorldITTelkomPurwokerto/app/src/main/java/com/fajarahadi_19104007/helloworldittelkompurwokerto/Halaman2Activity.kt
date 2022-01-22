b35f9eae780443c3d3e705ef1f9ee1769b8909fc
Muhammad Fajar Ahadi (19104007) <72422073+fajarahadi@users.noreply.github.com>
2022-01-22 19:59:10 +0700

    Modul3

---
+++ Modul3/HelloWorldITTelkomPurwokerto/app/src/main/java/com/fajarahadi_19104007/helloworldittelkompurwokerto/Halaman2Activity.kt
@@ -0,0 +1,11 @@
+package com.fajarahadi_19104007.helloworldittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Halaman2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman2)
+    }
+}
---
+++ Modul3/My First App/app/src/main/java/com/fajarahadi_19104007/myFirstApp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fajarahadi_19104007.myFirstApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
